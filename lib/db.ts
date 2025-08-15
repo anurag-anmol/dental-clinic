30c337be34cfde97a7448142b305e836773ac914
anuragMahehswari <maheshwarianurag792@gmail.com>
2025-08-15 12:34:05 +0530

    db changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise"
 
 const dbConfig = {
-  // host: "localhost",
+  host: "localhost",
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE_URL,
